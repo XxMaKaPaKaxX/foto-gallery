43385aeea607d12a41db605c3e8c1175db13e3d5
Max Lyovin <lyovin.maxim@gmail.com>
2021-01-12 12:18:38 +0100

    prepared class whit tools and initioation

---
+++ js/app.js
@@ -0,0 +1,40 @@
+import '../sass/style.scss';
+
+
+
+class DogBreeds {
+  constructor() {
+    this.apiUrl = 'https://dog.ceo/api';
+    this.imgEl = document.querySelector('.featured-dog img');
+
+    this.init();
+  }
+
+  getListBreeds = () => fetch(`${this.apiUrl}/breeds/list/all`)
+  .then((resp) => resp.json())
+  .then((data) => data.message);
+
+  getRandomImage = () => fetch(`${this.apiUrl}/breeds/image/random`)
+  .then((resp) => resp.json())
+  .then((data) => data.message);
+
+ getRandomImageByBreed = (breed) => fetch(`${this.apiUrl}/breed/${breed}/images/random`)
+  .then((resp) => resp.json())
+  .then((data) => data.message);
+
+  init = () => {
+    this.getRandomImage()
+      .then(src => {
+        console.log(src);
+        this.imgEl.setAttribute('src', src)
+      })
+    this.getListBreeds()
+      .then(list => console.log(list))
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const dogs = new DogBreeds;
+})
+
+
